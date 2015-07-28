61be44cb23cd526eae55f65ebcce1f732405b1ba
MICHAEL <milaske@gmail.com>
2015-07-28 16:35:29 -0500

    rcav_solution

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -1,5 +1,16 @@
 class CalculationsController < ApplicationController
+
   def instructions
-    render("instructions.html.erb")
+        render ("instructions.html.erb")
   end
+
+  def square
+
+    @numbers = params[:list_of_numbers].gsub(',', '').split.map(&:to_f)
+
+    @square = @numbers.square
+
+        render ("square.html.erb")
+  end
+
 end
